d26fa07b2f68223f89337276837d7630f3c5e204
LuisMallmannFecaf <luis.mallmann.92529@a.fecaf.com.br>
2025-04-13 22:04:43 -0300

    feat: Janela Gráfica -Kanban de Tarefas- closes #3

---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import messagebox
+
+# Cria a janela principal
+janela = tk.Tk()
+janela.title("Kanban de Tarefas")
+
+# Inicia a interface gráfica
+janela.mainloop()
